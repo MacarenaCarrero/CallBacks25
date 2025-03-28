e567cd66f170939b7a7e11a59e0855d6998b4522
macarena <macarena.c.rios@gmail.com>
2025-03-28 11:50:55 +0100

    CallBacks25

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// no se puede utilizar for ni for of
+
+// 1️⃣ Camila está organizando datos en su sistema. Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array. Ejemplo: Si recibe [2, 5, 8], deberá mostrar:
+
+//0 - 0
+//5 - 5
+//16 - 8
