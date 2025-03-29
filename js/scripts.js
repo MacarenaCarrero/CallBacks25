a88eea5d820595a338a51e0b441c9b093637154b
MacarenaCarrero <macarena.c.rios@gmail.com>
2025-03-29 13:30:26 +0100

    CallBacks25

--- js/scripts.js
+++ js/scripts.js
@@ -37,8 +37,46 @@ uppercaseWords(['hola', 'mundo']);
 
 // 4️⃣ Macarena está organizando una lista de ingredientes. Crea una función que reciba un array de palabras y una letra. La función devolverá un array con las palabras que comiencen por esa letra. Debe funcionar con mayúsculas y minúsculas. Ejemplo: Si recibe (["Manzana", "Melón", "Pera", "Sandía"], "m"), deberá imprimir ["Manzana", "Melón"].
 
-//const ingredientsList =
+const ingredientsList = (words, letter) => {
+  const result = words.filter(word => {
+    return word.charAt(0).toLowerCase() === letter.toLowerCase();
+  });
+  console.log(result);
+};
+ingredientsList(['Manzana', 'Melón', 'Pera', 'Sandía'], 'm');
 
 // 5️⃣ Camila está recopilando datos numéricos. Crea una función que reciba un array de 10 números e imprima por consola la suma de todos los valores del array. Ejemplo: Si recibe [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], deberá mostrar 55.
 
-const addNumbers = numbers => {};
+const addNumbers = numbers => {
+  const result = numbers.reduce((acc, number) => {
+    return acc + number;
+  });
+  console.log(result);
+};
+addNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+// 6️⃣ Bego está creando informes detallados. Investiga el método Math.pow() Crea una función que reciba un array de 10 números y muestre por consola cada número, su cuadrado y su cubo en este formato: "Número: 2 - Cuadrado: 4 - Cubo: 8".
+
+const powersOfNumbers = numbers => {
+  const result = numbers.map(number => {
+    const cuadrado = Math.pow(number, 2);
+    const cubo = Math.pow(number, 3);
+    console.log(`Número: ${number} - Cuadrado: ${cuadrado} - Cubo: ${cubo}`);
+  });
+  console.log(result);
+};
+powersOfNumbers([2, 4, 6, 8, 10, 12, 14, 16, 18, 20]);
+
+// 7️⃣ Sabrina quiere modificar un mensaje secreto. Crea una función que reciba una palabra e imprima por consola esa palabra pero con las vocales en mayúscula. Ejemplo: Si recibe "javascript", deberá imprimir "jAvAscrIpt".
+
+const secretCode = word => {
+  let result = word.map(letter => {
+    const vowels = 'aeiou';
+
+    if (vowels.includes(letter)) {
+      result += letter.toUpperCase();
+    }
+  });
+  console.log(result);
+};
+secretCode(['javascript']);
